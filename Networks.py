1512cbf78cb48c70d0e068903cb02c1db101d65c
jwhitlow45 <jwhitlow45@gmail.com>
2022-11-11 16:14:07 -0800

    feat:add board parameters and network structure

---
+++ Networks.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import keras.layers as tfl
+
+from board import BOARD_WIDTH, BOARD_HEIGHT
+
+def create_conv2d_model():
+    
+    model = tf.keras.Sequential()
+    model.add(tfl.Input((BOARD_HEIGHT, BOARD_WIDTH, 1)))
+    model.add(tfl.Conv2D(filters=64, kernel_size=5, padding='same', activation=None))
+    model.add(tfl.ReLU())
+    model.add(tfl.LayerNormalization())
+    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same', activation=None))
+    model.add(tfl.ReLU())
+    model.add(tfl.LayerNormalization())
+    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same', activation=None))
+    model.add(tfl.ReLU())
+    model.add(tfl.LayerNormalization())
+    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same', activation=None))
+    model.add(tfl.ReLU())
+    model.add(tfl.LayerNormalization())
+    model.add(tfl.Flatten())
+    model.add(tfl.Dense(64, activation=None))
+    model.add(tfl.LeakyReLU())
+    model.add(tfl.LayerNormalization())
+    model.add(tfl.Dense(64, activation=None))
+    model.add(tfl.LeakyReLU())
+    model.add(tfl.LayerNormalization())
+    model.add(tfl.Dense(64, activation=None))
+    model.add(tfl.LeakyReLU())
+    model.add(tfl.LayerNormalization())
+    model.add(tfl.Dense(BOARD_WIDTH, activation='linear'))
+    
+    model.compile(optimizer='adam')
+    
+    return model
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from ExperienceReplay import Experience, Memory
+import tensorflow as tf
 
+import Networks
+from board import BOARD_HEIGHT, BOARD_WIDTH
+
+TARGET_NET: tf.keras.Sequential = Networks.create_conv2d_model()
+PRIMARY_NET: tf.keras.Sequential = Networks.create_conv2d_model()
 
 def main():
     
+    
+    
     pass
+
 
 if __name__ == '__main__':
     main()
